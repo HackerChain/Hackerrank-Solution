5ca17a056da29417f0d9bf5ff213c8c5a07ba6f7
HackerChain <HackerChain>
2024-12-10 15:41:24 -0600

    [feat] 10 days of JS

---
+++ 10 days of Javascript/Hello, World.js
@@ -0,0 +1,7 @@
+function greeting(parameterVariable) {
+  // Print Hello, World!
+  console.log("Hello, World!");
+
+  // Print the contents of parameterVariable
+  console.log(parameterVariable);
+}
